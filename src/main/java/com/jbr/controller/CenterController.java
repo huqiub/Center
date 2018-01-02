4eb7d9c4d1316c0fbacad3a23c5a8a0d7aaf80e2
Jack <huqiub@gmail.com>
2018-01-02 11:08:24 +0800

    是

--- src/main/java/com/jbr/controller/CenterController.java
+++ src/main/java/com/jbr/controller/CenterController.java
@@ -1,9 +1,19 @@
 package com.jbr.controller;
 
-import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,9 +29,25 @@ public class CenterController {
 	@Autowired
 	private CenterService centerService;
 
+	@Value("${script.location}")
+	private String scriptLocation;
+
+	@Value("${script.name}")
+	private String scriptName;
+
 	@RequestMapping("/")
-	public String index() {
+	public String test() {
 		return "index";
+	}
+
+	@RequestMapping("/getDeviceIndex")
+	public String getDeviceIndex() {
+		return "getDevice";
+	}
+
+	@RequestMapping("/planIndex")
+	public String planIndex() {
+		return "plan";
 	}
 
 	@RequestMapping(value = "/getDevice", method = RequestMethod.POST)
@@ -47,4 +73,30 @@ public class CenterController {
 		return msg;
 	}
 
+	@RequestMapping(value = "/task/download/{id}", method = RequestMethod.GET)
+	public void downloadScript(@PathVariable int id, HttpServletRequest request, HttpServletResponse response) {
+
+		String fullPath = scriptLocation + (id + "_" + scriptName);
+		File downloadFile = new File(fullPath);
+
+		ServletContext context = request.getServletContext();
+
+		String mimeType = context.getMimeType(fullPath);
+		if (mimeType == null) {
+			mimeType = "application/octet-stream";
+		}
+		response.setContentType(mimeType);
+		response.setContentLength((int) downloadFile.length());
+
+		String headerKey = "Content-Disposition";
+		String headerValue = String.format("attachment; filename=\"%s\"", scriptName);
+		response.setHeader(headerKey, headerValue);
+		try {
+			InputStream myStream = new FileInputStream(fullPath);
+			IOUtils.copy(myStream, response.getOutputStream());
+			response.flushBuffer();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
---
+++ src/main/resources/scripts/1_test.lua
@@ -0,0 +1,22 @@
+fsdf saf
+fsd
+fa
+sadf
+ sd
+ f
+ asdf
+ sad
+ f
+ sadf
+ sd
+ fa
+ asf
+ s
+ df
+ sdf
+ sa
+ f
+ saf
+ saf
+ 
+ 
